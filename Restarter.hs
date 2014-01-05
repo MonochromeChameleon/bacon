0b7bcb6b45c75a908cb9ab6ed3361d577677745d
Hugh Greenish <github@monochromechameleon.com>
2014-01-05 18:07:09 +0000

    comments

--- Restarter.hs
+++ Restarter.hs
@@ -1,12 +1,19 @@
+-- | A handy little script for killing and restarting the bacon crawl process. I configured this
+-- | to run once a day on a cron job while I was on holiday, just in case the crawling process
+-- | hung at any point.
 import System.ShQQ
 import System.Cmd
 import Data.List
 import Data.Time
 
+-- | Kills and starts the crawl
+main :: IO ()
 main = do
     killProcess
     startProcess
 
+
+-- | Identify the crawl process and kill it
 killProcess :: IO ()
 killProcess = do
     putStrLn "Killing process"
@@ -14,12 +21,16 @@ killProcess = do
     system $ "kill " ++ processId
     return ()
     
+
+-- | Start a new crawl again
 startProcess :: IO ()
 startProcess = do
     putStrLn "And off we go again..."
     system "./Bacon crawl 6 +RTS -N4"
     return ()
     
+
+-- | Identifies the process id corresponding to the provided keywords
 getProcessId :: [String] -> IO String
 getProcessId greps = do
     let cmd = buildCommand "ps ax" greps
@@ -30,7 +41,8 @@ getProcessId greps = do
     else
         return ""
     
-    
+
+-- | Recursively builds up a process identifying console command with the desired grep filters    
 buildCommand :: String -> [String] -> String 
 buildCommand prefix [] = prefix
 buildCommand prefix (grep:greps) = buildCommand (prefix ++ "|grep " ++ grep) greps 
